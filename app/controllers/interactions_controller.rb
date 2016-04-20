eb42cb68e46209f58d34b8bad6e051b633c21043
Diego Llamas <diegollams@gmail.com>
2016-04-20 11:46:08 -0500

    bug fixed in error from for in interaction

--- app/controllers/interactions_controller.rb
+++ app/controllers/interactions_controller.rb
@@ -1,10 +1,9 @@
 class InteractionsController < ApplicationController
   before_action :set_interaction, only: [:show, :edit, :update, :destroy]
-
+  before_action :set_customer,only: [:index,:new]
   # GET /interactions
   # GET /interactions.json
   def index
-    @customer = Customer.find params[:customer_id]
     @interactions = Interaction.from_customer @customer
   end
 
@@ -20,13 +19,14 @@ class InteractionsController < ApplicationController
 
   # GET /interactions/1/edit
   def edit
+    @customer = @interaction.customer
   end
 
   # POST /interactions
   # POST /interactions.json
   def create
     @interaction = Interaction.new(interaction_params)
-
+    @customer = @interaction.customer
     respond_to do |format|
       if @interaction.save
         format.html { redirect_to @interaction, notice: 'Interaction was successfully created.' }
@@ -42,6 +42,7 @@ class InteractionsController < ApplicationController
   # PATCH/PUT /interactions/1.json
   def update
     respond_to do |format|
+      @customer = @interaction.customer
       if @interaction.update(interaction_params)
         format.html { redirect_to @interaction, notice: 'Interaction was successfully updated.' }
         format.json { render :show, status: :ok, location: @interaction }
@@ -55,14 +56,19 @@ class InteractionsController < ApplicationController
   # DELETE /interactions/1
   # DELETE /interactions/1.json
   def destroy
+
     @interaction.destroy
     respond_to do |format|
-      format.html { redirect_to interactions_url, notice: 'Interaction was successfully destroyed.' }
+      format.html { redirect_to interactions_url(customer_id: @interaction.customer_id), notice: 'Interaction was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
+
+    def set_customer
+      @customer = Customer.find params[:customer_id]
+    end
     # Use callbacks to share common setup or constraints between actions.
     def set_interaction
       @interaction = Interaction.find(params[:id])
