65acf650e138a747f93ef25902eb6c4cbcc40e2c
Roberto Ruiz-Velasco Martinez <rob@thebitcorps.com>
2016-04-21 20:10:46 -0500

    demo ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,7 +6,7 @@ class Ability
     #
     user ||= User.new # guest user (not logged in)
     if user.salesman?
-      can :manage, :all
+      can :manage, Lot
     else
       can :read, :all
     end
