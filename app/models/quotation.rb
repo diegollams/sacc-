1279ea0d3e90a2afce023c2a6122a2b69c0ccb93
Roberto Ruiz-Velasco Martinez <rob@thebitcorps.com>
2016-05-18 21:47:55 -0500

    go for it

--- app/models/quotation.rb
+++ app/models/quotation.rb
@@ -1,19 +1,18 @@
 class Quotation < ActiveRecord::Base
-  ######################### Validations ##########################
-  validates :unit_price,:square_meters,:customer_id, presence: true
-  ####################### Active Relations ########################
+  #-- Validations
+  validates :unit_price, :square_meters, :customer_id, :lot, presence: true
+  #-- Active Relations
   belongs_to :customer
   belongs_to :lot
-  has_one :salesman,through: :customer
-  #################### Class Variables #####################
+  has_one :salesman, through: :customer
+  #-- Class Variables
   VALID_AFTER = 15
   attr_reader :full_price
-  #################### Class Methods / Scopes #####################
+  #-- Class Methods / Scopes
   default_scope {order('created_at DESC')}
   scope :from_customer , ->(customer_id) {where( customer_id: customer_id).order(created_at: :asc)}
   scope :from_month, ->(date) {where(created_at: date.beginning_of_month..date.end_of_month)}
-
-  ####################### Instance Methods ########################
+  #-- Instance Methods
   def full_price
     unit_price * square_meters
   end
@@ -25,5 +24,4 @@ class Quotation < ActiveRecord::Base
   def still_valid?(date)
     date <= valid_until
   end
-
 end
