1a9e430a340a9307144355764517c4d9f70f7c20
Roberto Ruiz-Velasco Martinez <rob@thebitcorps.com>
2016-05-21 00:00:20 -0500

    combined link_to helper for sidebar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,19 @@
 module ApplicationHelper
   def check_fav(condition)
-     i_class = 'fa fa-'
-     i_class += condition ? 'check text-success' : 'times text-danger'
-    content_tag :i,'',class: i_class
+    i_class = 'fa fa-'
+    i_class += condition ? 'check text-success' : 'times text-danger'
+    content_tag :i, '', class: i_class
   end
 
   def check_active_link(path)
     current_page?(path) ? 'active' : '' 
+  end
+
+  def combo_link_to(path, icon = 'warning-exclamation', label = nil, klass = nil)
+    icn = content_tag(:i, '', class: "fa fa-#{icon} fa-fw")
+    lbl = label || klass.model_name.human.pluralize.titleize
+    link_to path, class: check_active_link(path) do
+      [icn, lbl].join.html_safe
+    end
   end
 end
