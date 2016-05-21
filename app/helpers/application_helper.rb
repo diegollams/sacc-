2bd74e159d62227d196b1f4b35a8db8eed51714a
Roberto Ruiz-Velasco Martinez <rob@thebitcorps.com>
2016-05-20 22:31:17 -0500

    check_active_link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
      i_class += condition ? 'check text-success' : 'times text-danger'
     content_tag :i,'',class: i_class
   end
+
+  def check_active_link(path)
+    current_page?(path) ? 'active' : '' 
+  end
 end
